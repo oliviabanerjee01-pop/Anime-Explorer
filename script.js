90df145cc501e239730e6cd45e20b7eb5529ed9f
oliviabanerjee01 pop <oliviabanerjee01@gmail.com>
2026-07-24 20:05:31 +0530

    Complete API integration and anime list rendering

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const inputBox = document.getElementById("input-box");
 const button = document.getElementById("button")
+const animeList = document.getElementById("anime-list");
+console.log(animeList);
 button.addEventListener(
     "click",
     addName
 );
 function addName(){
+    console.log(inputBox.value);
     if(inputBox.value === ''){
         alert("Please add a name");
     }
     else{
-        console.log(inputBox.value);
-    }
-}
+    fetch("https://api.jikan.moe/v4/top/anime")
+    .then(response => response.json())
+    .then(data => {
+        animeList.innerHTML = "";
+        
+        data.data.slice(0,5).forEach(anime =>{
+            animeList.innerHTML += `<p>${anime.title}</p>`;
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    }};
